0ac8140830815edb4213a0ab7e0b370043efc02b
Reid Philpot <reidphilpot@gmail.com>
2015-04-03 23:33:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# A bunch of static webservers
+
+## To use
+
+Start a server with one of the following:
+
+### Node
+
+```sh
+npm install
+node server.js
+```
+
+### Python
+
+```sh
+pip install -r requirements.txt
+python server.py
+```
---
+++ server.py
@@ -0,0 +1,8 @@
+import json
+from flask import Flask, Response, request
+
+app = Flask(__name__, static_url_path='', static_folder='public')
+app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
+
+if __name__ == '__main__':
+	app.run(port=3000)
